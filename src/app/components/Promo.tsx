f5e8479f92caeea1f9b7013e636e40a0b1c28631
Petar Hristovski <petarhristovski23@gmail.com>
2024-05-20 18:24:09 +0200

    adding link to the promo component

--- src/app/components/Promo.tsx
+++ src/app/components/Promo.tsx
@@ -3,6 +3,7 @@
 import { MdElectricBolt } from "react-icons/md";
 import SecondaryButton from "./SecondaryButton";
 import { RevealList } from "next-reveal";
+import Link from "next/link";
 
 export default function Promo() {
     return(
@@ -12,7 +13,11 @@ export default function Promo() {
             <div className="rounded bg-eestec md:px-12 py-6 px-8">
                 <h3 className="lg:text-6xl text-5xl text-white font-eestec font-semibold mt-6 mb-2">Want more <span>workshops</span> like this?</h3>
                 <p className="text-white font-eestec font-light mb-8">Join EESTEC LC Skopje and be the first to know about our upcoming events.</p>
-                <SecondaryButton text={"Get Involved Now!"} icon={MdElectricBolt}></SecondaryButton>
+                
+                <Link href={"https://www.instagram.com/eesteclcskopje/"}  target="_blank">
+                <SecondaryButton  text={"Get Involved Now!"} icon={MdElectricBolt}></SecondaryButton>
+                </Link>
+                
             </div>
         </RevealList>
         </section>
